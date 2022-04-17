73e20d8ab8cf8451834e1a83fcf531d82818882a
Luiz <luizfranzon05@hotmail.com>
2022-04-17 15:16:54 -0300

    Just Organizing

--- script.js
+++ script.js
@@ -1,39 +1,37 @@
-
-const timer = document.getElementById('timer');
+const timer = document.getElementById("timer");
 
 // Time Calc
 const destTime = new Date("feb 28, 2028 22:00:00").getTime();
 
 var now = new Date().getTime();
 
-    var difference = destTime - now ;
+var difference = destTime - now;
 
-    var days = Math.floor(difference / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60))
-    var seconds = Math.floor((difference % (1000 * 60)) / 1000 )
+var days = Math.floor(difference / (1000 * 60 * 60 * 24));
+var hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+var minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+var seconds = Math.floor((difference % (1000 * 60)) / 1000);
 
-    timer.innerText = `${days} days, ${hours} hours, ${minutes} minutes, ${seconds} seconds`
+timer.innerText = `${days} days, ${hours} hours, ${minutes} minutes, ${seconds} seconds`;
 
-    if (difference < 0) {
-        timer.innerText = "It's Out"
-    }
+if (difference < 0) {
+    timer.innerText = "It's Out";
+}
 
 // timer changer
-var timeCalc = setInterval(function() {
+var timeCalc = setInterval(function () {
     var now = new Date().getTime();
 
-    var difference = destTime - now ;
+    var difference = destTime - now;
 
     var days = Math.floor(difference / (1000 * 60 * 60 * 24));
     var hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60))
-    var seconds = Math.floor((difference % (1000 * 60)) / 1000 )
+    var minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((difference % (1000 * 60)) / 1000);
 
-    timer.innerText = `${days} days, ${hours} hours, ${minutes} minutes, ${seconds} seconds`;
+    timer.innerText = `${days} days, ${hours} hours, ${minutes} minutes and ${seconds} seconds`;
 
     if (difference < 0) {
-        timer.innerText = "It's Out"
+        timer.innerText = "It's Out";
     }
-
-}, 1000)
+}, 1000);
